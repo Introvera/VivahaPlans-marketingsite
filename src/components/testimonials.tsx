9ae987aed6c7cbbce2915527b5af196333c0beac
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-07-19 18:57:08 +0530

    Create testimonials.tsx

---
+++ src/components/testimonials.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Carousel from './Carousel'
+
+const howitwork = () => {
+  return (
+    <div className="mt-24 pl-16 z-20 pt-10 mb-20">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl text-[#2C5530] font-bold leading-tight">Testimonials</h1>
+        <h2 className="text-black w-60 mb-10 ml-1">Don’t just take our word for it! Hear it from our partners.</h2>
+      <Carousel />
+    </div>
+  )
+}
+
+export default howitwork
